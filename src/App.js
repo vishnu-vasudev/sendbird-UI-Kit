c42981de28fe2d9d8ec7fbc3f1df9d88f90b3bdc
vishnu-vasudev <vishnuviva97@gmail.com>
2022-11-17 15:44:02 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SendbirdApp from "@sendbird/uikit-react/App";
+import "@sendbird/uikit-react/dist/index.css";
+import SendbirdProvider from "@sendbird/uikit-react/SendbirdProvider";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <SendbirdApp
+        appId={"6419EB19-5274-4A82-A194-3AE6D8B8C4BF"}
+        userId={"User2"}
+        showSearchIcon
+        config={{
+          userMention: {
+            maxMentionCount: 10,
+            maxSuggestionCount: 15,
+          }
+        }}
+      />
+      <SendbirdProvider
+        appId={"6419EB19-5274-4A82-A194-3AE6D8B8C4BF"}
+        userId={"User2"}
+        isMentionEnabled
+        config={{
+          userMention: {
+            maxMentionCount: 10,
+            maxSuggestionCount: 15,
+          }
+        }}
+      />
+    </>
   );
 }
 
